5a7c06ddb134c3c42778c4a4d6364eda19f5683c
Willian Frigotto <willianfrigottofb@gmail.com>
2015-05-31 11:05:11 -0300

    Navegabilidade da tela Inicial

---
+++ app/src/main/java/br/edu/unisep/photomania/LoginActivity.java
@@ -0,0 +1,9 @@
+package br.edu.unisep.photomania;
+
+import android.app.Activity;
+
+/**
+ * Created by Willian on 31/05/2015.
+ */
+public class LoginActivity extends Activity{
+}
--- app/src/main/java/br/edu/unisep/photomania/MainActivity.java
+++ app/src/main/java/br/edu/unisep/photomania/MainActivity.java
@@ -1,9 +1,10 @@
 package br.edu.unisep.photomania;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +13,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+
     }
 
     @Override
@@ -21,18 +25,20 @@ public class MainActivity extends Activity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
+    public void logar(View v) {
+
+        startActivity(new Intent(getBaseContext(), LoginActivity.class));
+
+        finish();
     }
+
+    public void cadastrar(View v) {
+
+        startActivity(new Intent(getBaseContext(), CadastrarActivity.class));
+
+        finish();
+    }
+
 }
