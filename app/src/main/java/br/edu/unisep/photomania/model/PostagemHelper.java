bb2415b89748c404f35d4e388bcade4a9f2b8544
Willian Frigotto <willianfrigottofb@gmail.com>
2015-06-14 00:08:08 -0300

    commit

---
+++ app/src/main/java/br/edu/unisep/photomania/model/PostagemHelper.java
@@ -0,0 +1,19 @@
+package br.edu.unisep.photomania.model;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Willian on 13/06/2015.
+ */
+public class PostagemHelper extends SQLiteOpenHelper{
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/br/edu/unisep/photomania/model/UsuarioHelper.java
@@ -0,0 +1,34 @@
+package br.edu.unisep.photomania.model;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Willian on 13/06/2015.
+ */
+public class UsuarioHelper extends SQLiteOpenHelper {
+
+    public UsuarioHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String sql = "create table usuario (" +
+                " _id integer primary key, " +
+                " nome text, " +
+                " email text, " +
+                " senha text, " +
+                " dt_nascimento integer, " +
+                "caminho_foto text" +
+                " )";
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
